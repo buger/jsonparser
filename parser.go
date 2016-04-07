ec1f9d9886ea8a8fb579ba46562f007b399c92e0
Drew Boyuka <drew@pendo.io>
2016-04-07 15:49:21 -0400

    searchKeys and GetString now handle escaped strings

--- parser.go
+++ parser.go
@@ -21,6 +21,10 @@ var (
 	MalformedStringEscapeError = errors.New("Encountered an invalid escape sequence in a string")
 )
 
+// How much stack space to allocate for unescaping JSON strings; if a string longer
+// than this needs to be escaped, it will result in a heap allocation
+const unescapeStackBufSize = 64
+
 func tokenEnd(data []byte) int {
 	for i, c := range data {
 		switch c {
@@ -106,6 +110,8 @@ func searchKeys(data []byte, keys ...string) int {
 	ln := len(data)
 	lk := len(keys)
 
+	var stackbuf [unescapeStackBufSize]byte // stack-allocated array for allocation-free unescaping of small strings
+
 	for i < ln {
 		switch data[i] {
 		case '"':
@@ -128,14 +134,22 @@ func searchKeys(data []byte, keys ...string) int {
 
 			// if string is a Key, and key level match
 			if data[i] == ':' {
-				key := unsafeBytesToString(data[keyBegin:keyEnd])
+				key := data[keyBegin:keyEnd]
 
-				if keyLevel == level-1 && // If key nesting level match current object nested level
-					keys[level-1] == key {
-					keyLevel++
-					// If we found all keys in path
-					if keyLevel == lk {
-						return i + 1
+				// for unescape: if there are no escape sequences, this is cheap; if there are, it is a
+				// bit more expensive, but causes no allocations unless len(key) > unescapeStackBufSize
+				if keyUnesc, err := Unescape(key, stackbuf[:]); err != nil {
+					return -1
+				} else {
+					keyUnescStr := unsafeBytesToString(keyUnesc)
+
+					if keyLevel == level-1 && // If key nesting level match current object nested level
+						keys[level-1] == keyUnescStr {
+						keyLevel++
+						// If we found all keys in path
+						if keyLevel == lk {
+							return i + 1
+						}
 					}
 				}
 			} else {
@@ -373,9 +387,10 @@ func GetString(data []byte, keys ...string) (val string, err error) {
 		return string(v), nil
 	}
 
-	s, err := strconv.Unquote(`"` + unsafeBytesToString(v) + `"`)
+	var stackbuf [unescapeStackBufSize]byte // stack-allocated array for allocation-free unescaping of small strings
+	out, err := Unescape(v, stackbuf[:])
 
-	return s, err
+	return string(out), err
 }
 
 // GetFloat returns the value retrieved by `Get`, cast to a float64 if possible.
