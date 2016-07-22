9b126cd5c21ea05ef9d3bc759d39608ab53c79d3
Drew Boyuka <drew@pendo.io>
2016-07-21 23:15:58 -0400

    ValueType now implements Stringer for easier debugging

--- parser.go
+++ parser.go
@@ -346,6 +346,27 @@ const (
 	Unknown
 )
 
+func (vt ValueType) String() string {
+	switch vt {
+	case NotExist:
+		return "non-existent"
+	case String:
+		return "string"
+	case Number:
+		return "number"
+	case Object:
+		return "object"
+	case Array:
+		return "array"
+	case Boolean:
+		return "boolean"
+	case Null:
+		return "null"
+	default:
+		return "unknown"
+	}
+}
+
 var (
 	trueLiteral  = []byte("true")
 	falseLiteral = []byte("false")
