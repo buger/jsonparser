03905a4326684bce6801fe33b357376fbfe1d6dc
Drew Boyuka <drew@pendo.io>
2016-08-03 23:54:18 -0400

    Added ObjectEach benchmarks, expanded/improved EachKey benchmarks

--- benchmark/benchmark_small_payload_test.go
+++ benchmark/benchmark_small_payload_test.go
@@ -16,6 +16,8 @@ import (
 	"github.com/ugorji/go/codec"
 	"testing"
 	// "fmt"
+	"bytes"
+	"errors"
 )
 
 // Just for emulating field access, so it will not throw "evaluated but not used"
@@ -36,15 +38,15 @@ func BenchmarkJsonParserSmall(b *testing.B) {
 }
 
 func BenchmarkJsonParserEachKeyManualSmall(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		paths := [][]string{
-			[]string{"uuid"},
-			[]string{"tz"},
-			[]string{"ua"},
-			[]string{"st"},
-		}
+	paths := [][]string{
+		[]string{"uuid"},
+		[]string{"tz"},
+		[]string{"ua"},
+		[]string{"st"},
+	}
 
-		jsonparser.EachKey(smallFixture, func(idx int, value []byte, vt jsonparser.ValueType, err error){
+	for i := 0; i < b.N; i++ {
+		jsonparser.EachKey(smallFixture, func(idx int, value []byte, vt jsonparser.ValueType, err error) {
 			switch idx {
 			case 0:
 				// jsonparser.ParseString(value)
@@ -59,18 +61,18 @@ func BenchmarkJsonParserEachKeyManualSmall(b *testing.B) {
 	}
 }
 
-
 func BenchmarkJsonParserEachKeyStructSmall(b *testing.B) {
+	paths := [][]string{
+		[]string{"uuid"},
+		[]string{"tz"},
+		[]string{"ua"},
+		[]string{"st"},
+	}
+
 	for i := 0; i < b.N; i++ {
-		paths := [][]string{
-			[]string{"uuid"},
-			[]string{"tz"},
-			[]string{"ua"},
-			[]string{"st"},
-		}
 		var data SmallPayload
 
-		jsonparser.EachKey(smallFixture, func(idx int, value []byte, vt jsonparser.ValueType, err error){
+		jsonparser.EachKey(smallFixture, func(idx int, value []byte, vt jsonparser.ValueType, err error) {
 			switch idx {
 			case 0:
 				data.Uuid, _ = jsonparser.ParseString(value)
@@ -84,6 +86,44 @@ func BenchmarkJsonParserEachKeyStructSmall(b *testing.B) {
 				data.St = int(v)
 			}
 		}, paths...)
+
+		nothing(data.Uuid, data.Tz, data.Ua, data.St)
+	}
+}
+
+func BenchmarkJsonParserObjectEachStructSmall(b *testing.B) {
+	uuidKey, tzKey, uaKey, stKey := []byte("uuid"), []byte("tz"), []byte("ua"), []byte("st")
+	errStop := errors.New("stop")
+
+	for i := 0; i < b.N; i++ {
+		var data SmallPayload
+
+		missing := 4
+
+		jsonparser.ObjectEach(smallFixture, func(key, value []byte, vt jsonparser.ValueType, off int) error {
+			switch {
+			case bytes.Equal(key, uuidKey):
+				data.Uuid, _ = jsonparser.ParseString(value)
+				missing--
+			case bytes.Equal(key, tzKey):
+				v, _ := jsonparser.ParseInt(value)
+				data.Tz = int(v)
+				missing--
+			case bytes.Equal(key, uaKey):
+				data.Ua, _ = jsonparser.ParseString(value)
+				missing--
+			case bytes.Equal(key, stKey):
+				v, _ := jsonparser.ParseInt(value)
+				data.St = int(v)
+				missing--
+			}
+
+			if missing == 0 {
+				return errStop
+			} else {
+				return nil
+			}
+		})
 
 		nothing(data.Uuid, data.Tz, data.Ua, data.St)
 	}
